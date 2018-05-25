6f22d69c312240f95ce3e8ec02c1c70484eec805
Anillab <anillabusolo@gmail.com>
2018-05-26 01:09:53 +1245

    modified 2 files,added one file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Quote} from './quote';
 
 @Component({
   selector: 'app-root',
@@ -6,12 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes:string[]
-  constructor(){
-    this.quotes=["code more","text less",
-    "Money  doesn not create happiness but surely makes misery easier to live with",
-    "Ideas are owned with Time"
+  quotes=[
+    new Quote(" KarlMax","Code more text less","Max"),
+    new Quote("Igoni" ,"Money  does not create happiness but surely makes misery easier to live with","Anilla"),
+    new Quote("Musk","Ideas are owned with Time","Kim"),
+    new Quote("Obama","Change is what we believe in","Karanja"),
 
-    ];
+  ]
+  constructor(){
+    
   }
 }
