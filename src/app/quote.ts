e7e9b5a74593cdbd707f5f688536974f2f159de4
Anillab <anillabusolo@gmail.com>
2018-05-27 06:31:10 +1245

    Added a date pipe to my time function

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,11 +1,10 @@
 export class Quote {
   public upVotes:number
   public downVotes:number
-  public postedTime:Date
-  constructor(public quote:string,public author:string,public writerQuote:string)
+  constructor(public quote:string,public author:string,public writerQuote:string,public postedTime:Date
+)
   {
   this.upVotes=0;
   this.downVotes=0
-  this.postedTime=new Date()
 }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,10 +8,10 @@ import {Quote} from '../quote'
 })
 export class QuoteComponent implements OnInit {
   quotes=[
-    new Quote(" KarlMax","Code more text less","Max"),
-    new Quote("Igoni" ,"Money  does not create happiness but surely makes misery easier to live with","Anilla"),
-    new Quote("Musk","Ideas are owned with Time","Kim"),
-    new Quote("Obama","Change is what we believe in","Karanja"),
+    new Quote(" KarlMax","Code more text less","Max",new Date,(2017,0,18)),
+    new Quote("Igoni" ,"Money  does not create happiness but surely makes misery easier to live with","Anilla",new Date(1997,8,30)),
+    new Quote("Musk","Ideas are owned with Time","Kim",new Date(2016,7,12)),
+    new Quote("Obama","Change is what we believe in","Karanja",new Date(2015,6,17)),
 
   ]
   addVote(i){
