8465325ae66c78e5fe36cc37ebf824393b623f17
Anillab <anillabusolo@gmail.com>
2018-05-28 20:14:16 +1245

    pushing dist folder

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,8 +1,7 @@
 export class Quote {
   public upVotes:number
   public downVotes:number
-  constructor(public quote:string,public author:string,public writerQuote:string,public postedTime:Date
-)
+  constructor(public quote:string, public author:string, public writerQuote:string, public postedTime:Date)
   {
   this.upVotes=0;
   this.downVotes=0;
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Quote } from '../quote'
+import {Quote} from '../quote'
 
 @Component({
   selector: 'app-quote',
@@ -8,7 +8,7 @@ import { Quote } from '../quote'
 })
 export class QuoteComponent implements OnInit {
   quotes=[
-    new Quote(" KarlMax","Code more text less","Max",new Date,(2017,0,18)),
+    new Quote(" KarlMax","Code more text less","Max",new Date(2017,0,18)),
     new Quote("Igoni" ,"Money  does not create happiness but surely makes misery easier to live with","Anilla",new Date(1997,8,30)),
     new Quote("Musk","Ideas are owned with Time","Kim",new Date(2016,7,12)),
     new Quote("Obama","Change is what we believe in","Karanja",new Date(2015,6,17)),
